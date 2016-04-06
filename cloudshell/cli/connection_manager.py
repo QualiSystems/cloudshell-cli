c95377dba01915f3ca0c7c1f54ee0fe087983d75
Yaroslav Nikonorov <yaroslav.n@qualisystems.com>
2016-04-06 14:50:56 +0300

    Fixed Multiple MC

--- cloudshell/cli/connection_manager.py
+++ cloudshell/cli/connection_manager.py
@@ -4,7 +4,6 @@ from Queue import Queue
 from threading import Lock
 
 import inject
-from cloudshell.cli.helper.singleton import Singleton
 import types
 
 _CREATE_SESSION_LOCK = Lock()
@@ -41,16 +40,16 @@ class ReturnToPoolProxy(object):
         return getattr(self._instance, name)
 
     def __del__(self):
-        if ConnectionManager.is_defined():
-            print('Run Session Deleted')
-            cm = ConnectionManager()
-            print(cm)
+        if inject and inject.is_configured():
+            logger = inject.instance('logger')
+            logger.debug('Del was called')
+            cm = inject.instance('connection_manager')
             cm.return_session_to_pool(self)
 
 
-class ConnectionManager(Singleton):
-    def __init__(self):
+class ConnectionManager(object):
 
+    def __init__(self):
         self._config = inject.instance('config')
         self._connection_map = self._config.CONNECTION_MAP
 
@@ -65,6 +64,8 @@ class ConnectionManager(Singleton):
         self._connection_type = self._config.CONNECTION_TYPE
 
         self._pool_timeout = self._config.POOL_TIMEOUT
+        self.logger = inject.instance('logger')
+        self.logger.debug('Connection manager created')
 
     def _new_session(self, connection_type, logger=None, context=None, api=None):
         """
@@ -77,7 +78,7 @@ class ConnectionManager(Singleton):
         if connection_type in self._connection_map:
             session_object = self._connection_map[connection_type].create_session(context, api)
             session_object.connect(re_string=self._prompt)
-            logger.info('Created new session')
+            logger.debug('Created new session')
         else:
             raise Exception('ConnectionManager', 'Connection type {0} have not defined'.format(connection_type))
 
@@ -123,7 +124,6 @@ class ConnectionManager(Singleton):
     @inject.params(logger='logger')
     def _get_session_from_pool(self, logger=None):
         try:
-            print('waiting for session')
             session_object = self._session_pool.get(True, self._pool_timeout)
             logger.info('Get session from pool')
         except Exception as error_object:
@@ -136,7 +136,7 @@ class ConnectionManager(Singleton):
         if not time_out:
             time_out = self._pool_timeout
         self._session_pool.put(session, True, time_out)
-        logger.info('Return session back to pool')
+        logger.debug('Return session back to pool')
 
     def get_session_instance(self, logger=None):
         """
@@ -148,14 +148,14 @@ class ConnectionManager(Singleton):
 
         with _CREATE_SESSION_LOCK:
             if self._session_pool.empty() and self._created_session_count < self._max_connections:
-                session = self._create_session_by_connection_type()
+                self.return_session_to_pool(self._create_session_by_connection_type())
                 self._created_session_count += 1
-            else:
-                session = self._get_session_from_pool()
-        return session
+        return self._get_session_from_pool()
+
+    def get_pool_size(self):
+        return self._session_pool.qsize()
 
     @staticmethod
-    def get_session():
-        cm = ConnectionManager()
-        print(cm)
-        return cm.get_session_instance()
+    @inject.params(connection_manager='connection_manager')
+    def get_session(connection_manager=None):
+        return connection_manager.get_session_instance()
