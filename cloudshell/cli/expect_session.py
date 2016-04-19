071da9567853a9947ba2ceffc7d95ab881eeb280
CoYe <wiseman.kostya@gmail.com>
2016-04-19 12:14:12 +0300

    changed exception string in connection manager and send_line method in expect session

--- cloudshell/cli/expect_session.py
+++ cloudshell/cli/expect_session.py
@@ -49,9 +49,7 @@ class ExpectSession(Session):
 
         return current_output
 
-    @inject.params(logger='logger')
-    def send_line(self, data_str, logger=None):
-        logger.debug(data_str)
+    def send_line(self, data_str):
         self._send(data_str + self._new_line)
 
     @inject.params(logger='logger')
@@ -69,6 +67,7 @@ class ExpectSession(Session):
         """
 
         if data_str is not None:
+            logger.info('Send command: ' + data_str)
             self.send_line(data_str)
 
         if re_string is None or len(re_string) == 0:
@@ -83,6 +82,7 @@ class ExpectSession(Session):
         output_list = list()
         while True:
             if re.search(re_string, output_str, re.DOTALL):
+                logger.info('Output is:\n{0}'.format(output_str))
                 break
             else:
                 time.sleep(0.2)
