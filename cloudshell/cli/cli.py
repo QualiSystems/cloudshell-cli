b13f47f1a9055f32654033d0d598080747e99f01
LuizaNacshon <luiza.n@quali.com>
2016-09-20 20:13:51 +0300

    added changes - need to discuss about the prompt tree

--- cloudshell/cli/cli.py
+++ cloudshell/cli/cli.py
@@ -11,27 +11,20 @@ class Cli(object):
 
         self.logger = Logger('logger')
 
-    def new_session(self,session_type,ip,default_mode,port='',user='',password=''):
+    def new_session(self,session_type,ip,default_state,port='',user='',password=''):
         session_handler = SessionHandler()
-        session,connection_manager = session_handler.initiate_connection_manager(self.logger,session_type,ip,port,user,password,default_mode)
-        return SessionModeWrapper(session, connection_manager,default_mode)
-
-
-
-
-
-
-
+        session,connection_manager = session_handler.initiate_connection_manager(self.logger,session_type,ip,port,user,password,default_state)
+        return SessionModeWrapper(session, connection_manager,default_state)
 
 
 if __name__ == '__main__':
     #default_mode = Command_mode('[>$#]/s*$', 'enter', 'exit')
+    from cloudshell.cli.cli import Cli
+    from cloudshell.cli.prompt import Prompt
     import paramiko
 
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('192.168.28.150', 22, 'root', 'Juniper')
     cli=Cli()
-    with cli.new_session(session_type=Cli.SSH,ip='192.168.28.150',user='root',password='Juniper', default_mode = 'root@%') as default_session:
-        print default_session
-        default_session.send_command('show version',state.default)
+    default_state = Prompt('root@%', 'enter', 'exit')
+    with cli.new_session(session_type=Cli.SSH,ip='192.168.28.150',user='root',password='Juniper', default_mode = default_state) as session:
+        session.send_command('show version')
+        session.change_mode()
