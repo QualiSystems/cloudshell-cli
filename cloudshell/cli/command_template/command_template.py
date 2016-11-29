2150be5171465397982b1f757855da537a8ec442
Yaroslav Nikonorov <yaroslav.n@qualisystems.com>
2016-11-29 11:26:43 +0200

    Added command template executor

--- cloudshell/cli/command_template/command_template.py
+++ cloudshell/cli/command_template/command_template.py
@@ -1,20 +1,107 @@
 from collections import OrderedDict
+
 import re
+from cloudshell.cli.cli_service import CliService
+
+
+class CommandTemplateExecutor(object):
+    """
+    Execute command template using cli service
+    """
+
+    def __init__(self, cli_service, command_template):
+        """
+        :param cli_service:
+        :type cli_service: CliService
+        :param command_template:
+        :type command_template: CommandTemplate
+        :return:
+        """
+        self._cli_service = cli_service
+        self._command_template = command_template
+        self._action_map = OrderedDict()
+        self._error_map = OrderedDict()
+
+    @property
+    def action_map(self):
+        """
+        Return updated action
+        """
+        return self._action_map.copy().update(self._command_template.action_map)
+
+    @property
+    def error_map(self):
+        return self._command_template.error_map.copy().update(self._error_map)
+
+    def execute_command(self, **command_kwargs):
+        command = self._command_template.get_raw_command(**command_kwargs)
+        self._cli_service.send_command(command, action_map=self.action_map, error_map=self.error_map)
+
+    def update_action_map(self, action_map):
+        self._action_map.update(action_map)
+
+    def update_error_map(self, error_map):
+        self._error_map.update(error_map)
 
 
 class CommandTemplate:
-    def __init__(self, command, action_map=None, error_map=None):
-        self._command = command
-        self._action_map = action_map or OrderedDict()
-        self._error_map = error_map or OrderedDict()
+    """
+    Command template class
+    """
 
-    def get_command(self, **kwargs):
-        action_map = (OrderedDict(kwargs.get('action_map', None) or OrderedDict()))
-        action_map.update(self._action_map)
-        error_map = OrderedDict(self._error_map)
-        error_map.update(kwargs.get('error_map', None) or OrderedDict())
+    def __init__(self, command, action_map=OrderedDict(), error_map=OrderedDict()):
+        """
+        :param command:
+        :type command: basestring
+        :param action_map:
+        :type action_map: OrderedDict
+        :param error_map:
+        :type error_map: OrderedDict
+        """
+        self._command = command
+        self._action_map = action_map
+        self._error_map = error_map
+
+    @property
+    def action_map(self):
+        """
+        :rtype: dict
+        """
+        return self._action_map
+
+    @property
+    def error_map(self):
+        """
+        :rtype: dict
+        """
+        return self._error_map
+
+    def get_raw_command(self, **command_kwargs):
+        """
+        Generate command string from command template
+        :param command_kwargs:
+        :type command_kwargs: dict
+        :return:
+        :rtype: basestring
+        """
+        return self._prepare_command(**command_kwargs)
+
+    def get_command(self, action_map=OrderedDict(), error_map=OrderedDict(), **command_kwargs):
+        """
+        Generate dict for cli_service signature with action map and error map
+        :param action_map:
+        :type action_map: dict
+        :param error_map:
+        :type error_map: dict
+        :param command_kwargs:
+        :type command_kwargs: dict
+        :return:
+        :rtype: dict
+        """
+        action_map.update(self.action_map)
+        error_map.update(self.error_map)
         return {
-            'command': self._prepare_command(**kwargs),
+            'command': self._prepare_command(**command_kwargs),
             'action_map': action_map,
             'error_map': error_map
         }
