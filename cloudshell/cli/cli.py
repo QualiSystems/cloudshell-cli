7f8dacbba4a6d3fb9b008289ed56e71777fb28c0
Yaroslav Nikonorov <yaroslav.n@quali.com>
2020-06-15 14:18:21 +0300

    Added deprication warning

--- cloudshell/cli/cli.py
+++ cloudshell/cli/cli.py
@@ -1,4 +1,12 @@
 """Deprecated,CLI moved to cloudshell.cli.service.cli.CLI."""
+import warnings
+
 from cloudshell.cli.service.cli import CLI
 
+warnings.warn(
+    "Module cloudshell.cli.cli moved to cloudshell.cli.service.cli",
+    category=DeprecationWarning,
+    stacklevel=2,
+)
+warnings.simplefilter("default", DeprecationWarning)
 __all__ = [CLI]
