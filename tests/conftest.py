6729575476ac00c6d64bae2d4c0b64bfd780ec1d
Kyrylo Maksymenko <saklar13@gmail.com>
2023-03-16 12:21:46 +0100

    update getting supported sessions for the CLI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import logging
-import sys
 
 import pytest
 
@@ -7,9 +6,3 @@ import pytest
 @pytest.fixture()
 def logger():
     return logging.getLogger(__file__)
-
-
-def pytest_ignore_collect(path):
-    if sys.version_info.major == 2:
-        if str(path).endswith("_py3.py"):
-            return True
